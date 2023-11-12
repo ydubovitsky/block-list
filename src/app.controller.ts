6b4cd4f4ace1ef0c13062045d0434f5cd8562927
y-dubovitsky <pXmJ5M9dvP>
2023-11-12 13:56:52 +0300

    Prisma and db service added

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ApiOkResponse, ApiProperty } from '@nestjs/swagger';
+import { DbService } from './db/db.service';
 
 class HelloWorldDto {
   @ApiProperty()
@@ -9,13 +10,18 @@ class HelloWorldDto {
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) { }
+  constructor(
+    private readonly appService: AppService,
+    private dbService: DbService,
+  ) {}
 
   @Get()
   @ApiOkResponse({
     type: HelloWorldDto,
   })
-  getHello(): HelloWorldDto {
+  async getHello(): Promise<HelloWorldDto> {
+    const users = await this.dbService.user.findMany({});
+    console.log(users);
     return { message: this.appService.getHello() };
   }
 }
